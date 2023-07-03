86fc271dbd709e3bba57b46b68a8d347603caa7f
Asanali Esmagambetov <supersanin45@gmail.com>
2023-07-03 16:13:27 +0600

    My first commit

--- src/test/java/com/example/tgbotinjava/TgBotInJavaApplicationTests.java
+++ src/test/java/com/example/tgbotinjava/TgBotInJavaApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TgBotInJavaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
