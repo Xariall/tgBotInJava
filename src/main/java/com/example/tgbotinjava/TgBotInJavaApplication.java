b626ab3a908ef5bfd7707010e81ee4528f5be6e5
Asanali Esmagambetov <supersanin45@gmail.com>
2023-07-03 16:12:45 +0600

    first commit

---
+++ src/main/java/com/example/tgbotinjava/TgBotInJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.tgbotinjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgBotInJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgBotInJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tgbotinjava/TgBotInJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tgbotinjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgBotInJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
